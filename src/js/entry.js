d2f911225e9ddb7b38bf1a90809201f47027cfc5
TevinThuku <tevinthuku@gmail.com>
2016-06-29 11:32:35 +0300

    Add all files

---
+++ src/js/entry.js
@@ -0,0 +1,9 @@
+//import css
+import '../css/master.scss';
+
+//import react and JS
+import HelloBox from './HelloBox';
+import React from 'react';
+
+// render!!
+React.render(<HelloBox />, document.getElementsByTagName('body')[0]);
